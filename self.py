042798c85f0ac3b9164e45d89b1db6a2f77cfa44
不知天高地厚的颜某人 <123466895+06yanm@users.noreply.github.com>
2024-07-19 12:09:04 +0800

    Update self.py

--- self.py
+++ self.py
@@ -5,7 +5,7 @@ url = "https://live.zhoujie218.top/tv/iptv4.txt"
 r = requests.get(url)
 r.encoding = "utf-8"
 
-new = ""
+new = []
 
 if r.status_code == 200:
     line = r.text.split("\n")
@@ -13,7 +13,7 @@ if r.status_code == 200:
         if "更新时间" in li:
             break
         else:
-            new = new + str(li) +"\n"
-            print(li)
-    with open("./self.txt", "w", encoding="utf-8") as file:
-        file.write(new)
+            new.append(li)
+with open("./self.txt", "w", encoding="utf-8") as file:
+        for i in new:
+            file.write(i + "\n")
